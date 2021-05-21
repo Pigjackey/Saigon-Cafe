b62bb1a656bee128a29019ada1240e742faaac55
Donovon Spencer <Donovon.Spencer@gmail.com>
2021-05-20 20:34:08 -0600

    Added items and sorted lots of prices and stuff

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -398,6 +398,187 @@ Drinks…………………………...….$1.95<br>
             { name: 'BBQ Pork', number: 11, price: '$6.95', hot: false, category: 'appetizers' },
             { name: 'Pot Stickers (6)', number: 12, price: '$4.95', hot: false, category: 'appetizers' },
           ]
+        },
+        {
+          id: 3, name: 'Fried Rice', chinese: '', show: false, children: [
+            { name: 'Chicken Fried Rice', number: 13, price: '$6.25', hot: false, category: 'friedRice' },
+            { name: 'Roast Pork Fried Rice', number: 14, price: '$6.95', hot: false, category: 'friedRice' },
+            { name: 'Ham Fried Rice', number: 15, price: '$5.95', hot: false, category: 'friedRice' },
+            { name: 'Beef Fried Rice', number: 16, price: '$6.95', hot: false, category: 'friedRice' },
+            { name: 'Shrimp Fried Rice', number: 17, price: '$7.50', hot: false, category: 'friedRice' },
+            { name: 'House Special Fried Rice', number: 18, price: '$7.50', hot: false, category: 'friedRice' },
+            { name: 'Hong Kong Style Fried Rice', number: 19, price: '$7.50', hot: false, category: 'friedRice' },
+          ]
+        },
+        {
+          id: 4, name: 'Pan Fried Noodles', chinese: '', show: false, children: [
+            { name: 'Chicken Pan Fried Noodles', number: 20, price: '$7.95', hot: false, category: 'friedNoodles' },
+            { name: 'BBQ Pork Pan Fried Noodles', number: 21, price: '$8.50', hot: false, category: 'friedNoodles' },
+            { name: 'Vegetable Pan Fried Noodles', number: 22, price: '$7.25', hot: false, category: 'friedNoodles' },
+            { name: 'Beef Pan Fried Noodles', number: 23, price: '$8.50', hot: false, category: 'friedNoodles' },
+            { name: 'Shrimp Pan Fried Noodles', number: 24, price: '$8.95', hot: false, category: 'friedNoodles' },
+            { name: 'House Special Pan Fried Noodles', number: 25, price: '$8.95', hot: false, category: 'friedNoodles' },
+          ]
+        },
+        {
+          id: 5, name: 'Lo Mein', chinese: '', show: false, children: [
+            { name: 'Chicken Lo Mein', number: 26, price: '$6.75', hot: false, category: 'loMein' },
+            { name: 'Roast Pork Lo Mein', number: 27, price: '$7.25', hot: false, category: 'loMein' },
+            { name: 'Vegetable Lo Mein', number: 28, price: '$6.25', hot: false, category: 'loMein' },
+            { name: 'Beef Lo Mein', number: 29, price: '$7.25', hot: false, category: 'loMein' },
+            { name: 'Shrimp Lo Mein', number: 30, price: '$7.75', hot: false, category: 'loMein' },
+            { name: 'House Special Lo Mein', number: 31, price: '$7.75', hot: false, category: 'loMein' },
+          ]
+        },
+        {
+          id: 6, name: 'Crunchy Chow Mein', chinese: '', show: false, children: [
+            { name: 'Chicken Chow Mein', number: 32, price: '$6.75', hot: false, category: 'chowMein' },
+            { name: 'Roast Pork Chow Mein', number: 33, price: '$7.25', hot: false, category: 'chowMein' },
+            { name: 'Vegetable Chow Mein', number: 34, price: '$6.25', hot: false, category: 'chowMein' },
+            { name: 'Beef Chow Mein', number: 35, price: '$7.25', hot: false, category: 'chowMein' },
+            { name: 'Shrimp Chow Mein', number: 36, price: '$7.75', hot: false, category: 'chowMein' },
+            { name: 'House Special Chow Mein', number: 37, price: '$7.75', hot: false, category: 'chowMein' },
+          ]
+        },
+        {
+          id: 7, name: 'Chicken', chinese: '', tooltip: '(comes with steamed rice)', show: false, children: [
+            { name: 'Pot Stickers (6)', number: 38, price: '$7.50', hot: false, category: 'chicken' },
+            { name: 'Pot Stickers (6)', number: 39, price: '$7.50', hot: false, category: 'chicken' },
+            { name: 'Pot Stickers (6)', number: 40, price: '$7.50', hot: false, category: 'chicken' },
+            { name: 'Pot Stickers (6)', number: 41, price: '$7.50', hot: false, category: 'chicken' },
+            { name: 'Pot Stickers (6)', number: 42, price: '$7.50', hot: false, category: 'chicken' },
+            { name: 'Pot Stickers (6)', number: 43, price: '$7.50', hot: false, category: 'chicken' },
+            { name: 'Pot Stickers (6)', number: 44, price: '$7.50', hot: false, category: 'chicken' },
+            { name: 'Pot Stickers (6)', number: 45, price: '$7.50', hot: false, category: 'chicken' },
+            { name: 'Pot Stickers (6)', number: 46, price: '$7.50', hot: false, category: 'chicken' },
+            { name: 'Pot Stickers (6)', number: 47, price: '$7.50', hot: false, category: 'chicken' },
+            { name: 'Pot Stickers (6)', number: 48, price: '$7.50', hot: false, category: 'chicken' },
+            { name: 'Pot Stickers (6)', number: 49, price: '$7.50', hot: false, category: 'chicken' },
+            { name: 'Pot Stickers (6)', number: 50, price: '$7.50', hot: false, category: 'chicken' },
+            { name: 'Pot Stickers (6)', number: 51, price: '$7.50', hot: false, category: 'chicken' },
+            { name: 'Pot Stickers (6)', number: 52, price: '$7.50', hot: false, category: 'chicken' },
+            { name: 'Pot Stickers (6)', number: 53, price: '$7.50', hot: false, category: 'chicken' },
+          ]
+        },
+        {
+          id: 8, name: 'Beef', chinese: '', tooltip: '(comes with steamed rice)', show: false, children: [
+            { name: 'Pot Stickers (6)', number: 54, price: '$8.25', hot: false, category: 'beef' },
+            { name: 'Pot Stickers (6)', number: 55, price: '$8.25', hot: false, category: 'beef' },
+            { name: 'Pot Stickers (6)', number: 56, price: '$8.25', hot: false, category: 'beef' },
+            { name: 'Pot Stickers (6)', number: 57, price: '$8.25', hot: false, category: 'beef' },
+            { name: 'Pot Stickers (6)', number: 58, price: '$8.25', hot: false, category: 'beef' },
+            { name: 'Pot Stickers (6)', number: 59, price: '$8.25', hot: false, category: 'beef' },
+            { name: 'Pot Stickers (6)', number: 60, price: '$8.25', hot: false, category: 'beef' },
+            { name: 'Pot Stickers (6)', number: 61, price: '$8.25', hot: false, category: 'beef' },
+            { name: 'Pot Stickers (6)', number: 62, price: '$8.25', hot: false, category: 'beef' },
+            { name: 'Pot Stickers (6)', number: 63, price: '$8.25', hot: false, category: 'beef' },
+          ]
+        },
+        {
+          id: 9, name: 'Pork', chinese: '', tooltip: '(comes with steamed rice)', show: false, children: [
+            { name: 'Pot Stickers (6)', number: 64, price: '$7.50', hot: false, category: 'pork' },
+            { name: 'Pot Stickers (6)', number: 65, price: '$7.50', hot: false, category: 'pork' },
+            { name: 'Pot Stickers (6)', number: 66, price: '$7.50', hot: false, category: 'pork' },
+            { name: 'Pot Stickers (6)', number: 67, price: '$7.50', hot: false, category: 'pork' },
+            { name: 'Pot Stickers (6)', number: 68, price: '$7.50', hot: false, category: 'pork' },
+            { name: 'Pot Stickers (6)', number: 69, price: '$7.50', hot: false, category: 'pork' },
+            { name: 'Pot Stickers (6)', number: 70, price: '$7.50', hot: false, category: 'pork' },
+            { name: 'Pot Stickers (6)', number: 71, price: '$7.50', hot: false, category: 'pork' },
+          ]
+        },
+        {
+          id: 10, name: 'Shrimp', chinese: '', tooltip: '(comes with steamed rice)', show: false, children: [
+            { name: 'Pot Stickers (6)', number: 72, price: '$8.95', hot: false, category: 'shrimp' },
+            { name: 'Pot Stickers (6)', number: 73, price: '$8.95', hot: false, category: 'shrimp' },
+            { name: 'Pot Stickers (6)', number: 74, price: '$8.95', hot: false, category: 'shrimp' },
+            { name: 'Pot Stickers (6)', number: 75, price: '$8.95', hot: false, category: 'shrimp' },
+            { name: 'Pot Stickers (6)', number: 76, price: '$8.95', hot: false, category: 'shrimp' },
+            { name: 'Pot Stickers (6)', number: 77, price: '$8.95', hot: false, category: 'shrimp' },
+            { name: 'Pot Stickers (6)', number: 78, price: '$8.95', hot: false, category: 'shrimp' },
+            { name: 'Pot Stickers (6)', number: 79, price: '$8.95', hot: false, category: 'shrimp' },
+            { name: 'Pot Stickers (6)', number: 80, price: '$8.95', hot: false, category: 'shrimp' },
+          ]
+        },
+        {
+          id: 11, name: 'Vegetable', chinese: '', tooltip: '(comes with steamed rice)', show: false, children: [
+            { name: 'Pot Stickers (6)', number: 81, price: '$6.95', hot: false, category: 'vegetable' },
+            { name: 'Pot Stickers (6)', number: 82, price: '$6.95', hot: false, category: 'vegetable' },
+            { name: 'Pot Stickers (6)', number: 83, price: '$6.95', hot: false, category: 'vegetable' },
+            { name: 'Pot Stickers (6)', number: 84, price: '$6.75', hot: false, category: 'vegetable' },
+            { name: 'Pot Stickers (6)', number: 85, price: '$6.75', hot: false, category: 'vegetable' },
+          ]
+        },
+        {
+          id: 12, name: 'Egg Foo Young', chinese: '', tooltip: '(comes with steamed rice)', show: false, children: [
+            { name: 'Pot Stickers (6)', number: 86, price: '$6.95', hot: false, category: 'eggFoo' },
+            { name: 'Pot Stickers (6)', number: 87, price: '$7.25', hot: false, category: 'eggFoo' },
+            { name: 'Pot Stickers (6)', number: 88, price: '$6.75', hot: false, category: 'eggFoo' },
+            { name: 'Pot Stickers (6)', number: 89, price: '$7.25', hot: false, category: 'eggFoo' },
+            { name: 'Pot Stickers (6)', number: 90, price: '$7.65', hot: false, category: 'eggFoo' },
+            { name: 'Pot Stickers (6)', number: 91, price: '$6.75', hot: false, category: 'eggFoo' },
+          ]
+        },
+        {
+          id: 13, name: 'Sizzling Platter', chinese: '', tooltip: '(comes with steamed rice)', show: false, children: [
+            { name: 'Pot Stickers (6)', number: 92, price: '$7.95', hot: false, category: 'sizzling' },
+            { name: 'Pot Stickers (6)', number: 93, price: '$8.75', hot: false, category: 'sizzling' },
+            { name: 'Pot Stickers (6)', number: 94, price: '$8.95', hot: false, category: 'sizzling' },
+            { name: 'Pot Stickers (6)', number: 95, price: '$9.95', hot: false, category: 'sizzling' },
+          ]
+        },
+        {
+          id: 14, name: 'Noodle Soup', chinese: '', tooltip: '(Egg Noodle additional $1.00)', show: false, children: [
+            { name: 'Pot Stickers (6)', number: 96, price: '$6.75', hot: false, category: 'noodleSoup' },
+            { name: 'Pot Stickers (6)', number: 97, price: '$5.95', hot: false, category: 'noodleSoup' },
+            { name: 'Pot Stickers (6)', number: 98, price: '$6.25', hot: false, category: 'noodleSoup' },
+            { name: 'Pot Stickers (6)', number: 99, price: '$6.25', hot: false, category: 'noodleSoup' },
+            { name: 'Pot Stickers (6)', number: 100, price: '$6.75', hot: false, category: 'noodleSoup' },
+            { name: 'Pot Stickers (6)', number: 101, price: '$5.95', hot: false, category: 'noodleSoup' },
+            { name: 'Pot Stickers (6)', number: 102, price: '$7.25', hot: false, category: 'noodleSoup' },
+          ]
+        },
+        {
+          id: 15, name: 'Chef Specials', chinese: '', show: false, children: [
+            { name: 'Pot Stickers (6)', number: 103, price: '$9.50', hot: false, category: 'specials' },
+            { name: 'Pot Stickers (6)', number: 104, price: '$9.50', hot: false, category: 'specials' },
+            { name: 'Pot Stickers (6)', number: 105, price: '$9.50', hot: false, category: 'specials' },
+            { name: 'Pot Stickers (6)', number: 106, price: '$12.95', hot: false, category: 'specials' },
+            { name: 'Pot Stickers (6)', number: 107, price: '$11.95', hot: false, category: 'specials' },
+            { name: 'Pot Stickers (6)', number: 108, price: '$11.95', hot: false, category: 'specials' },
+          ]
+        },
+        {
+          id: 16, name: 'Special Combinations', chinese: '', show: false, children: [
+            { name: 'Pot Stickers (6)', number: 1, price: '$4.95', hot: false, category: 'combination' },
+            { name: 'Pot Stickers (6)', number: 2, price: '$4.95', hot: false, category: 'combination' },
+            { name: 'Pot Stickers (6)', number: 3, price: '$4.95', hot: false, category: 'combination' },
+            { name: 'Pot Stickers (6)', number: 4, price: '$4.95', hot: false, category: 'combination' },
+            { name: 'Pot Stickers (6)', number: 5, price: '$4.95', hot: false, category: 'combination' },
+            { name: 'Pot Stickers (6)', number: 6, price: '$4.95', hot: false, category: 'combination' },
+            { name: 'Pot Stickers (6)', number: 7, price: '$4.95', hot: false, category: 'combination' },
+            { name: 'Pot Stickers (6)', number: 8, price: '$4.95', hot: false, category: 'combination' },
+            { name: 'Pot Stickers (6)', number: 9, price: '$4.95', hot: false, category: 'combination' },
+            { name: 'Pot Stickers (6)', number: 10, price: '$4.95', hot: false, category: 'combination' },
+            { name: 'Pot Stickers (6)', number: 11, price: '$4.95', hot: false, category: 'combination' },
+            { name: 'Pot Stickers (6)', number: 12, price: '$4.95', hot: false, category: 'combination' },
+            { name: 'Pot Stickers (6)', number: 13, price: '$4.95', hot: false, category: 'combination' },
+            { name: 'Pot Stickers (6)', number: 14, price: '$4.95', hot: false, category: 'combination' },
+          ]
+        },
+        {
+          id: 17, name: 'Family Value Meals', chinese: '', show: false, children: [
+            { name: 'Pot Stickers (6)', price: '$4.95', hot: false, category: 'family' },
+          ]
+        },
+        {
+          id: 18, name: 'Side Orders', chinese: '', show: false, children: [
+            { name: 'Pot Stickers (6)', price: '$1.50', hot: false, category: 'side' },
+            { name: 'Pot Stickers (6)', price: '$3.00', hot: false, category: 'side' },
+            { name: 'Pot Stickers (6)', price: '$2.00', hot: false, category: 'side' },
+            { name: 'Pot Stickers (6)', price: '$1.50', hot: false, category: 'side' },
+            { name: 'Pot Stickers (6)', price: '$1.50', hot: false, category: 'side' },
+            { name: 'Pot Stickers (6)', price: '$1.95', hot: false, category: 'side' },
+          ]
         }
       ]
     }),
